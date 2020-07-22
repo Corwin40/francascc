91ab3437bd3d648a05357a472f80f7644a77ba67
Xavier BURKE <xavier.burke@gmail.com>
2020-07-22 18:54:54 +0200

    Symfony App - Navbar focntionelle
    Appel de la page

--- src/Controller/Webapp/AppController.php
+++ src/Controller/Webapp/AppController.php
@@ -2,21 +2,41 @@
 
 namespace App\Controller\Webapp;
 
+use App\Entity\Webapp\Page;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class AppController
  * @package App\Controller\Webapp
- * @Route("/",name="op_webapp_app")
+ * @Route("/",name="op_webapp_app_")
  */
 class AppController extends AbstractController
 {
     /**
-     * @Route("/", name="webapp_app_index")
+     * @Route("/", name="index")
      */
     public function index()
     {
         return $this->render('webapp/app/index.html.twig');
+    }
+
+    /**
+     * Affiche la page de menu
+     * @Route("/{slug}", name="page")
+     */
+    public function showPage($slug)
+    {
+        $page = $this->getDoctrine()
+            ->getRepository(Page::class)
+            ->findOneBy(['slug' => $slug]);
+
+        if (!$page) {
+            throw $this->createNotFoundException(
+                "La page n'existe pas" .$slug
+            );
+        }
+
+        return $this->render('webapp/app/page.html.twig');
     }
 }
