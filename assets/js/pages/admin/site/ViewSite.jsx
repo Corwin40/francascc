eba4ac207ff1360360e4ae942f16424ddc828709
Xavier BURKE <xavier.burke@gmail.com>
2020-07-29 08:27:23 +0200

    React - modifiation page config

--- assets/js/pages/admin/site/ViewSite.jsx
+++ assets/js/pages/admin/site/ViewSite.jsx
@@ -6,6 +6,7 @@ import moment from "moment";
 import Checkbox from "../../../components/forms/Checkbox";
 import SiteAPI from "../../../services/admin/ConfigAPI";
 import {toast} from "react-toastify";
+import Nav from "react-bootstrap/Nav";
 
 const ViewSite = ({match, history}) => {
 
@@ -78,60 +79,80 @@ const ViewSite = ({match, history}) => {
 
     return (
         <>
-            <div className="d-flex justify-content-between align-items-content mb-3">
-                {!editing && <h1>Paramétrage du site</h1> || <h1>Modification de l'utilisateur</h1>}
-            </div>
-            <form onSubmit={handleSubmit}>
-                <Field
-                    name="name"
-                    label="Nom du site"
-                    placeholder="Entrer le prénom"
-                    type="text"
-                    value={site.name}
-                    onChange={handleChange}
-                    error={errors.name}
-                />
-                <Field
-                    name="description"
-                    label="description du site"
-                    placeholder=""
-                    type="text"
-                    value={site.lastName}
-                    onChange={handleChange}
-                    error={errors.lastName}
-                />
-
-                {!editing &&
-                <Checkbox
-                    name="isActive"
-                    label = "Activation de l'utilisateur"
-                    isOn={value}
-                    handleToggle={({currentTarget}) => {
-                        setValue(!value);
-                        const {name} = currentTarget;
-                        setSite({... site, [name]:value});
-                    }}
-                />
-                ||
-                <Checkbox
-                    name="isOffline"
-                    label = "Mettre le site en maintenance"
-                    isOn={site.isOffline}
-                    handleToggle={({currentTarget}) => {
-                        setValue(!site.isOffline);
-                        const {name} = currentTarget;
-                        setSite({... site, [name]:value});
-                    }}
-                />
-                }
-
-                <div className="form-group">
-                    {!editing && <button className="btn btn-sm btn-success mr-1">Ajouter</button> || <button className="btn btn-sm btn-success mr-1">Modifier</button>}
-                    <Link to="/users" className="btn btn-sm btn-secondary">Retour à la liste</Link>
+            <nav>
+                <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a className="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+                       role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+                    <a className="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+                       role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+                    <a className="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
+                       role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
                 </div>
+            </nav>
+            <div className="tab-content" id="nav-tabContent">
+                <div
+                    className="tab-pane fade show active"
+                    id="nav-home"
+                    role="tabpanel"
+                    aria-labelledby="nav-home-tab">
+                    <form onSubmit={handleSubmit}>
+                        <Field
+                            name="name"
+                            label="Nom du site"
+                            placeholder="Entrer le prénom"
+                            type="text"
+                            value={site.name}
+                            onChange={handleChange}
+                            error={errors.name}
+                        />
+                        <Field
+                            name="description"
+                            label="description du site"
+                            placeholder=""
+                            type="text"
+                            value={site.lastName}
+                            onChange={handleChange}
+                            error={errors.lastName}
+                        />
+
+                        {!editing &&
+                        <Checkbox
+                            name="isActive"
+                            label = "Activation de l'utilisateur"
+                            isOn={value}
+                            handleToggle={({currentTarget}) => {
+                                setValue(!value);
+                                const {name} = currentTarget;
+                                setSite({... site, [name]:value});
+                            }}
+                        />
+                        ||
+                        <Checkbox
+                            name="isOffline"
+                            label = "Mettre le site en maintenance"
+                            isOn={site.isOffline}
+                            handleToggle={({currentTarget}) => {
+                                setValue(!site.isOffline);
+                                const {name} = currentTarget;
+                                setSite({... site, [name]:value});
+                            }}
+                        />
+                        }
+
+                        <div className="form-group">
+                            {!editing && <button className="btn btn-sm btn-success mr-1">Ajouter</button> || <button className="btn btn-sm btn-success mr-1">Modifier</button>}
+                            <Link to="/users" className="btn btn-sm btn-secondary">Retour à la liste</Link>
+                        </div>
 
 
-            </form>
+                    </form>
+                </div>
+                <div className="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...
+                </div>
+                <div className="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...
+                </div>
+            </div>
+
         </>
     )
 };
