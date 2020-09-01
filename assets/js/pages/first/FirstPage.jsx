3e988780cd61274e1be8d59b72bd63e62a47b5fd
xavier burke <xavier.burke@openpixl.fr>
2020-09-01 16:58:40 +0200

    REact first installation

--- assets/js/pages/first/FirstPage.jsx
+++ assets/js/pages/first/FirstPage.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {Button, Card, Col, Form, Row} from "react-bootstrap";
 
 
 const FirstPage = () => {
     return (
         <div className="row">
             <div className="col-12">
-                <h2>Introduction</h2>
-                <hr/>
-                <p>Bonjour,<br/>En cliquant sur le bouton "Suivant", vous allez suivre les étapes de configuration de votre nouveau site.</p>
-            </div>
-            <div className="col-12">
-                <Link className="btn btn-sm btn-primary float-right" to="/conf">Suivant</Link>
+                <Card>
+                    <Card.Body>
+                        <div className="row">
+                            <div className="col-12">
+                                <h2>Introduction</h2>
+                                <hr/>
+                                <p>Bonjour,<br/>En cliquant sur le bouton "Suivant", vous allez suivre les étapes de configuration de votre nouveau site.</p>
+                                <p>Pensez à noter vos identifiants dans un endroit sûr afin de les récupérer.</p>
+                            </div>
+                            <div className="col-12">
+                                <Link className="btn btn-sm btn-primary float-right" to="/conf">Suivant</Link>
+                            </div>
+                        </div>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
+
 
     );
 };
